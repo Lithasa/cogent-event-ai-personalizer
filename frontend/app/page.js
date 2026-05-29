16836fa518b944328d5a8d83c9cc022024a0ab2c
Lithasa Jayamaha <lithasa0417@gmail.com>
2026-05-29 17:35:12 +0530

    Fix frontend home page layout for Vercel deployment

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,26 +1,23 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import About from '../components/About'
-import Speakers from '../components/Speakers'
 import Agenda from '../components/Agenda'
+import Speakers from '../components/Speakers'
 import RegisterForm from '../components/RegisterForm'
 import Footer from '../components/Footer'
 import FloatingScrollTop from '../components/FloatingScrollTop'
 
 export default function Home() {
   return (
-    <>
-      <main>
-        <Navbar />
-        <Hero />
-        <About />
-        <Speakers />
-        <Agenda />
-        <RegisterForm />
-        <Footer />
-      </main>
-
+    <main>
+      <Navbar />
+      <Hero />
+      <About />
+      <Agenda />
+      <Speakers />
+      <RegisterForm />
+      <Footer />
       <FloatingScrollTop />
-    </>
+    </main>
   )
 }
